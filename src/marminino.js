7fb5e2ec1ddc2d788b97c426eacd0c9ac5283e16
rodrigsneto <contact@rodrigsneto.com>
2024-03-02 15:58:46 -0300

    resolucao

--- src/marminino.js
+++ src/marminino.js
@@ -1,5 +1,23 @@
 function executar(numero) {
-    // yourcode
+
+    resposta = ""
+
+    if (numero % 3 == 0) {
+        resposta += "Mar";
+    }
+
+    if (numero % 4 == 0) {
+        resposta += "Minino";
+    }
+
+    return resposta;
+    
+}
+
+i = 1;
+
+for (i; i <= 100; i++) {
+    console.log(i + " = " + executar(i));
 }
 
 module.exports = executar;
