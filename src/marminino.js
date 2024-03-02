d9018a5a9b344b004e7a14fe9969710dd07564b0
Kelvys Moura <kelvysmoura2@gmail.com>
2024-03-02 13:58:38 -0300

    removendo solução

--- src/marminino.js
+++ src/marminino.js
@@ -1,19 +1,5 @@
 function executar(numero) {
-    let resposta = "";
-
-    if (numero % 3 === 0) { 
-        resposta += "Mar";
-    }
-
-    if (numero % 4 === 0) {
-        resposta += "Minino";
-    }
-
-    return resposta;
-}
-
-for (let i = 1; i <= 100; i++) {
-    console.log(i + ": " + executar(i));
+    // yourcode
 }
 
 module.exports = executar;
